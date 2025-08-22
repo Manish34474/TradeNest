3559d6ef9c4fab13502070d089f06ec5d46921a9
Manish34474 <manishsah3459@gmail.com>
2025-08-22 07:25:31 +0545

    Init: Initialization of frontend for TradeNest

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col justify-center items-center gap-5 text-3xl font-extrabold bg-background">
+        TradeNest
+        <Button>Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
